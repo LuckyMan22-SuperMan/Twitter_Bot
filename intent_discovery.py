0a4311f3564a5a1d73e20163e7c5caf930fbd1df
Lakshya Agrawal <lakshyaus4@gmail.com>
2026-09-12 14:58:29 +0530

    basic checks bf implementing kmeans alog

--- intent_discovery.py
+++ intent_discovery.py
@@ -6,3 +6,20 @@ from sklearn.cluster import KMeans
 df = pd.read_csv("AppleSupport_conversations.csv")
 
 print("Dataset shape:", df.shape)
+
+texts = df["clean_customer_text"].fillna("")
+
+valid_mask = texts.str.strip() != ""
+
+texts = texts[valid_mask]
+
+print("Usable customer messages:", len(texts))
+
+vectorizer = TfidfVectorizer(
+    stop_words="english",
+    max_features=1000
+)
+
+X = vectorizer.fit_transform(texts)
+
+print("TF-IDF matrix shape:", X.shape)
