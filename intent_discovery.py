f8fe89baeb526bd154b0876bde6a3977e7e1fb0b
Lakshya Agrawal <lakshyaus4@gmail.com>
2026-09-16 09:08:54 +0530

    cluster classification 2

--- intent_discovery.py
+++ intent_discovery.py
@@ -40,3 +40,29 @@ clusters = kmeans.fit_predict(X)
 
 print("\nCluster sizes:")
 print(pd.Series(clusters).value_counts().sort_index())
+
+# --------------------------------------------------
+# Show important words for each cluster
+# --------------------------------------------------
+
+terms = vectorizer.get_feature_names_out()
+
+print("\n" + "=" * 80)
+print("IMPORTANT WORDS BY CLUSTER")
+print("=" * 80)
+
+for cluster_number in range(NUM_CLUSTERS):
+
+    center = kmeans.cluster_centers_[cluster_number]
+
+    top_indices = center.argsort()[-10:][::-1]
+
+    top_words = terms[top_indices]
+
+    print(
+        f"\nCluster {cluster_number}:"
+    )
+
+    print(
+        ", ".join(top_words)
+    )
